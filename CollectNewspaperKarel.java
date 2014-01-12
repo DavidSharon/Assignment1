d664e4dcc7cdcb5c88a7b6537a7f2692d23713e7
S <s>
2014-01-12 11:27:59 -0800

    {"type":"TI", "runs":"1"}

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -12,12 +12,25 @@ import stanford.karel.*;
 
 public class CollectNewspaperKarel extends Karel {
 	
-	// You fill in this part
+	/* CollectNewsPaperKarel class allows Karel to collect a beeper
+	* and return it to her (or his?) starting point in a pre-defined world
+	* Pre condition: Karel is in the world CollectNewsPaper facing East
+	* Post condition: Karel has one beeper in the Northwest corner of
+	* the inner box facing East */
+	
 	public void run() {
 		gettoTarget();
 		pickBeeper();
 		gettoTarget();
 	}
+	
+	/* gettoTarget makes Karel travel to or from these two points:
+	 * A: Northwest corner of inner box
+	 * B: One space outside of inner box where beeper is located
+	 * Precondition: Karel is facing the point to where travel is desired 
+	 * Postcondition: Karel is facing the point of origination
+	 */
+	
 	private void gettoTarget() {
 		move();
 		move();
@@ -28,11 +41,15 @@ public class CollectNewspaperKarel extends Karel {
 		turnAround();
 	}
 	
+	//* Makes Karel turn right
+	
 	private void turnRight() {
 		turnLeft();
 		turnLeft();
 		turnLeft();
 	}
+	
+	//* Makes Karel turn around
 	
 	private void turnAround() {
 		turnLeft();
